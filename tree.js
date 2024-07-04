ffadc3525bdf1d7b461bb7c26966923e9fdfe0ff
Sanup Divakaran <sanukyr@gmail.com>
2024-07-04 17:25:14 +0530

    tree v1

---
+++ tree.js
@@ -0,0 +1,20 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  isEmpty() {
+    return this.root == null;
+  }
+}
+
+const bst = new BST();
+console.log(bst.isEmpty());
