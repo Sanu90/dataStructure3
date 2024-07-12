e91d98deeb47a63c6c9e9c6f9ab0003814ae7c45
Sanup Divakaran <sanukyr@gmail.com>
2024-07-12 09:19:43 +0530

    BST: delete function

--- bst.js
+++ bst.js
@@ -111,6 +111,31 @@ class BST {
       return this.max(root.right);
     }
   }
+
+  delete(value) {
+    this.root = this.deleteNode(this.root, value);
+    return `deleted`;
+  }
+
+  deleteNode(root, value) {
+    if (value < root.value) {
+      root.left = this.deleteNode(root.left, value);
+    } else if (value > root.value) {
+      root.right = this.deleteNode(root.right, value);
+    } else {
+      if (!root.left && !root.right) {
+        return null;
+      }
+      if (!root.left) {
+        return root.right;
+      } else if (!root.right) {
+        return root.left;
+      }
+      root.value = this.min(root.right);
+      root.right = this.deleteNode(root.right, root.value);
+    }
+    return root;
+  }
 }
 
 const bst = new BST();
