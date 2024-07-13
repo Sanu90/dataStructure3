92937a9e79ec4ede39e1310d05bffe606be9ce53
Sanup Divakaran <sanukyr@gmail.com>
2024-07-13 06:42:26 +0530

    updated BST

--- bst.js
+++ bst.js
@@ -1,3 +1,5 @@
+// BST //
+
 class Node {
   constructor(value) {
     this.value = value;
@@ -10,11 +12,9 @@ class BST {
   constructor() {
     this.root = null;
   }
-
   isEmpty() {
     return this.root == null;
   }
-
   insert(value) {
     const node = new Node(value);
     if (this.isEmpty()) {
@@ -41,7 +41,9 @@ class BST {
   }
 
   search(root, value) {
-    if (!root) return false;
+    if (!root) {
+      return false;
+    }
     if (root.value == value) return true;
     if (value < root.value) {
       return this.search(root.left, value);
@@ -51,35 +53,28 @@ class BST {
   }
 
   preOrder(root) {
-    if (!root) return false;
-
-    console.log("PRE-ORDER", root.value);
-    this.preOrder(root.left);
-    this.preOrder(root.right);
+    if (root) {
+      console.log(root.value);
+      this.preOrder(root.left);
+      this.preOrder(root.right);
+    }
   }
 
   inOrder(root) {
-    if (!root) return false;
-    this.inOrder(root.left);
-    console.log(root.value);
-    //array.push(root.value);
-    this.inOrder(root.right);
+    if (root) {
+      this.inOrder(root.left);
+      console.log(root.value);
+      this.inOrder(root.right);
+    }
   }
 
   postOrder(root) {
-    if (!root) return false;
-    this.postOrder(root.left);
-    this.postOrder(root.right);
-    console.log("POST_ORDER", root.value);
+    if (root) {
+      this.postOrder(root.left);
+      this.postOrder(root.right);
+      console.log(root.value);
+    }
   }
-
-  display() {
-    console.log(this.root);
-  }
-
-  //pre --> root,left,right.
-  //in --> left, root, right.
-  //post --> left,right, root.
 
   breadthFS() {
     let queue = [];
@@ -99,58 +94,83 @@ class BST {
   min(root) {
     if (!root.left) {
       return root.value;
-    } else {
-      return this.min(root.left);
     }
+    return this.min(root.left);
   }
 
   max(root) {
     if (!root.right) {
       return root.value;
-    } else {
-      return this.max(root.right);
     }
+    return this.max(root.right);
   }
 
-  delete(value) {
-    this.root = this.deleteNode(this.root, value);
-    return `deleted`;
+  height(root) {
+    if (!root) {
+      //  console.log("empty");
+      return 0;
+    }
+    let leftHeight = this.height(root.left);
+    let rightHeight = this.height(root.right);
+    return Math.max(leftHeight, rightHeight) + 1;
   }
 
-  deleteNode(root, value) {
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+    return 0;
+  }
+
+  removeNode(root, value) {
+    if (!root) {
+      console.log("Value not in BST");
+      return;
+    }
     if (value < root.value) {
-      root.left = this.deleteNode(root.left, value);
+      root.left = this.removeNode(root.left, value);
     } else if (value > root.value) {
-      root.right = this.deleteNode(root.right, value);
+      root.right = this.removeNode(root.right, value);
     } else {
       if (!root.left && !root.right) {
         return null;
       }
-      if (!root.left) {
-        return root.right;
-      } else if (!root.right) {
+      if (!root.right) {
         return root.left;
+      } else if (!root.left) {
+        return root.right;
       }
       root.value = this.min(root.right);
-      root.right = this.deleteNode(root.right, root.value);
+      root.right = this.removeNode(root.right, root.value);
     }
     return root;
+  }
+
+  display() {
+    console.log(this.root);
   }
 }
 
 const bst = new BST();
-//console.log("is tree empty? ", bst.isEmpty());
 bst.insert(10);
+bst.insert(20);
+bst.insert(4);
+bst.insert(3);
 bst.insert(5);
-bst.insert(2);
-bst.insert(9);
-bst.insert(19);
 bst.insert(15);
-console.log("is tree empty? ", bst.isEmpty());
-console.log(bst.search(bst.root, 2));
-//bst.display();
+bst.insert(40);
+bst.insert(8);
+console.log(bst.search(bst.root, 40));
+//bst.preOrder(bst.root);
 //bst.inOrder(bst.root);
-bst.breadthFS();
-
-console.log(bst.min(bst.root));
-console.log(bst.max(bst.root));
+//bst.display()
+//bst.postOrder(bst.root);
+//bst.breadthFS();
+console.log("**");
+console.log("small:", bst.min(bst.root));
+console.log("big:", bst.max(bst.root));
+console.log("**");
+console.log(bst.height(bst.root));
+console.log("**");
+bst.remove(20);
+//bst.inOrder(bst.root);
+bst.display();
+console.log(bst.height(bst.root));
